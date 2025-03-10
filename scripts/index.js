66114b14fd8577eca0fed861d351e51ffb7c0530
Anastasia Kalinina <kalinush.malinush@gmail.com>
2025-03-10 18:46:48 +0300

    ispravleno

--- scripts/index.js
+++ scripts/index.js
@@ -8,23 +8,32 @@ const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-initialCards.forEach( function (element) {
-    let cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__image').src = element.link;
-    cardElement.querySelector('.card__image').alt = element.name;
-
+function createCard(item, onDelete) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    cardImage.src = item.link;
+    cardTitle.textContent = item.name;
+  
     const deleteButton = cardElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', function () {
-        deleteCard(cardElement);
-    });
-
-    placesList.append(cardElement);
-});
+    deleteButton.addEventListener('click', () => onDelete(cardElement));
+  
+    return cardElement;
+  };
 
 // @todo: Функция удаления карточки
-function deleteCard(cardElement) {
+function DeleteCard(cardElement) {
     cardElement.remove();
-  }
+  };
 
+// @todo: Функция рендера карточки
+    function renderCard(cardElement) {
+    placesList.append(cardElement);
+  };
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((card) => {
+    const cardElement = createCard(card, DeleteCard);
+    renderCard(cardElement);
+  });
+
