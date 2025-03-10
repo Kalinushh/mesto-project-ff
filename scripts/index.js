ee1e56896b6600aaa0d3afe442b247cf0a716e7b
Anastasia Kalinina <kalinush.malinush@gmail.com>
2025-03-10 12:25:35 +0300

    sdacha

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+initialCards.forEach( function (element) {
+    let cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__image').alt = element.name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function () {
+        deleteCard(cardElement);
+    });
+
+    placesList.append(cardElement);
+});
+
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+    cardElement.remove();
+  }
+
 
 // @todo: Вывести карточки на страницу
